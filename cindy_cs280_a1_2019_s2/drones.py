ceafd9469d831442ae4c897e96b006ec202a55ad
cindy069shigexiaokeai <54125988+cindy069shigexiaokeai@users.noreply.github.com>
2019-08-16 11:12:06 +1200

    Add files via upload

---
+++ cindy_cs280_a1_2019_s2/drones.py
@@ -0,0 +1,107 @@
+'''
+upi: xgao069
+name: Xinyi Gao
+student ID: 429784284
+'''
+class Drone(object):
+    """ Stores details on a drone. """
+
+    def __init__(self, name, class_type=1, rescue=False):
+        self.id = 0
+        self.name = name
+        self.class_type = class_type
+        self.rescue = rescue
+        self.operator = None
+
+class DroneAction(object):
+    """ A pending action on the DroneStore. """
+
+    def __init__(self, drone, operator, commit_action):
+        self.drone = drone
+        self.operator = operator
+        self.messages = []
+        self._commit_action = commit_action
+        self._committed = False
+
+
+    def add_message(self, message):
+        """ Adds a message to the action. """
+        self.messages.append(message)
+
+    def is_valid(self):
+        """ Returns True if the action is valid, False otherwise. """
+        return len(self.messages) == 0
+
+    def commit(self):
+        """ Commits (performs) this action. """
+        if self._committed:
+            raise Exception("Action has already been committed")
+
+        self._commit_action(self.drone, self.operator)
+        self._committed = True
+
+
+class DroneStore(object):
+    """ DroneStore stores all the drones for DALSys. """
+
+    def __init__(self, conn=None):
+        self._drones = {}
+        self._last_id = 0
+        self._conn = conn
+
+    def add(self, drone):
+        """ Adds a new drone to the store. """
+        if drone.id in self._drones:
+            raise Exception('Drone already exists in store')
+        else:
+            self._last_id += 1
+            drone.id = self._last_id
+            self._drones[drone.id] = drone
+
+    def remove(self, drone):
+        """ Removes a drone from the store. """
+        if not drone.id in self._drones:
+            raise Exception('Drone does not exist in store')
+        else:
+            del self._drones[drone.id]
+
+    def get(self, id):
+        """ Retrieves a drone from the store by its ID. """
+        if not id in self._drones:
+            return None
+        else:
+            return self._drones[id]
+
+    def list_all(self):
+        """ Lists all the drones in the system. """
+        for drone in self._drones:
+            yield drone
+
+    def allocate(self, drone, operator):
+        """ Starts the allocation of a drone to an operator. """
+        action = DroneAction(drone, operator, self._allocate)
+        #1. An operator can only operate one drone.
+        if operator.drone != None:
+            action.add_message("Operator can only control one drone")
+        #2. The operator must hold the correct license.
+        if drone.class_type != operator.drone_license:
+            action.add_message("Operator does not have correct drone license")
+        #3. For a rescue drone, the operator must hold a rescue drone endorsement.
+        if drone.rescue == True:
+           if operator.rescue_endorsement == False:
+                action.add_message("Operator does not have rescue endorsement")
+        return action
+
+    def _allocate(self, drone, operator):
+        """ Performs the actual allocation of the operator to the drone. """
+        if operator.drone is not None:
+            # If the operator had a drone previously, we need to clean it so it does not
+            # hold an incorrect reference
+            operator.drone.operator = None
+        operator.drone = drone
+        drone.operator = operator
+        self.save(drone)
+
+    def save(self, drone):
+        """ Saves the drone to the database. """
+        pass    # TODO: we don't have a database yet
